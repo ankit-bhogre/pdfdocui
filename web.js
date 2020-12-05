2596036b78499ecae36351a5e1080a050b7a575d
suchit0011 <suchit0011@gmail.com>
2020-12-05 13:40:44 +0530

    new changes

--- web.js
+++ web.js
@@ -23,11 +23,28 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
 //start app 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
-app.listen(port, () => 
-  console.log(`App is listening on port ${port}.`)
-);
+// app.listen(port, () => 
+//   console.log(`App is listening on port ${port}.`)
+// );
+const http = require('http')
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World! NodeJS \n');
+// });
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+app.get('/servertest', async (req, res) => {
+  res.send({server:"working"})
+})
 
 app.get('/getpdf', async (req, res) => {
 
